dacc5a3763782f6e535218eb893eacc01ae40a11
Mark Hahn <mark@hahnca.com>
2021-12-22 16:50:47 -0800

    moved config files to folder

--- index.js
+++ index.js
@@ -5,20 +5,16 @@ const exec     = util.promisify(cp.exec);
 import express from 'express';
 const  app     = new express();
 
-const header    = fs.readFileSync('config-hdr.txt',     'utf8');
-const footer    = fs.readFileSync('config-footer.txt',  'utf8');
-const seriesStr = fs.readFileSync('config-series.json', 'utf8');
+const header    = fs.readFileSync('config/config-hdr.txt',     'utf8');
+const footer    = fs.readFileSync('config/config-footer.txt',  'utf8');
+const seriesStr = fs.readFileSync('config/config-series.json', 'utf8');
 const series    = JSON.parse(seriesStr);
 
-const dwnLoadCmd = 'rsync -av xobtlu@oracle.usbx.me:' +
-                   '/home/xobtlu/.config/flexget/config.yml config.bkup';
-
-const upLoadCmd = 'rsync -av config.yml xobtlu@oracle.usbx.me:' +
-                   '/home/xobtlu/.config/flexget/config.yml';
-
 const upload = async () => {
+  const upLoadCmd = 'rsync -av config/config.yml xobtlu@oracle.usbx.me:' +
+                    '/home/xobtlu/.config/flexget/config.yml';
   const {stdout} = await exec(
-    'rsync -av config.yml xobtlu@oracle.usbx.me:' +
+    'rsync -av config/config.yml xobtlu@oracle.usbx.me:' +
     '/home/xobtlu/.config/flexget/config.yml');
   const rx = new RegExp('total size is ([0-9,]*)');
   const matches = rx.exec(stdout);
@@ -43,22 +39,52 @@ const reload = async () => {
 
 app.get('/', function (req, res) {
   res.send('invalid url')
-})
+});
+
+const saveSeries = async () => {
+  console.log('saving series');
+  fs.writeFileSync('series.json', JSON.stringify(series));
+  let str = header;
+  for(let name of series)
+    str += '        - "' + name.replace('"', '') + '"\n';
+  str += footer;
+  fs.writeFileSync('config/config.yml', str);
+  await upload();
+  await reload();
+};
 
 app.get('/config-series.json', function (req, res) {
-  res.send(fs.readFileSync('config-series.json', 'utf8'));
-})
-
+  res.send(fs.readFileSync('config/config-series.json', 'utf8'));
+});
+  
 app.post('/pickup/:name', function (req, res) {
   const name = req.params.name;
-  console.log('add series', name);
-  res.send('ok')
+  try{
+    if(!series.includes(name))
+      series.push(name);
+    saveSeries();
+    console.log('added series', name);
+    res.send('ok')
+  }
+  catch (e) {
+    console.log('ERROR: add series', name, e.message);
+    res.send(e.message);
+  }
 })
 
 app.delete('/pickup/:name', function (req, res) {
   const name = req.params.name;
-  console.log('delete series', name);
-  res.send('ok')
+  try{
+    const idx = series.indexOf(name);
+    if (idx !== -1) series.splice(idx, 1);
+    saveSeries();
+    console.log('removed series', name);
+    res.send('ok')
+  }
+  catch (e) {
+    console.log('ERROR: remove series', name, e.message);
+    res.send(e.message);
+  }
 })
 
 app.listen(8734, () => {
